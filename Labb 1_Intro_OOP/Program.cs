a1ee272a89cd46972ab371d6fda4d4d0ff14550f
Bellanthie <bellanthie@gmail.com>
2025-10-28 15:43:31 +0100

    I rewrote Radius with a capital 'R' in order to differentiate the computer's notes with the User's input (little 'r' in radius)

--- Labb 1_Intro_OOP/Program.cs
+++ Labb 1_Intro_OOP/Program.cs
@@ -7,8 +7,15 @@
         // 1) place the constructor within the curly brackets
         // 2) the Constructor is like the builder who knows how to create a new object:
         // It takes in the parameter (radius) and saves it in a 'field variable'
-        double radius; 
+        // **Within the class we also find our METHODS
+        public double Radius { get; set; } 
+        public Circle(double radius)
+        {
+            Radius = radius;// the name radius is for the computer to know. "r" is for the user to know so to speak
 
+        }
+
+        public 
 
 
     }
