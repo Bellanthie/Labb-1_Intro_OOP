2e1ed1339a4e9177685c8e13b70bd3f85373b60d
Bellanthie <bellanthie@gmail.com>
2025-10-28 15:20:04 +0100

    Add project files.

---
+++ Labb 1_Intro_OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Labb_1_Intro_OOP
+{
+
+    class Circle
+    {
+        // 1) place the constructor within the curly brackets
+        // 2) the Constructor is like the builder who knows how to create a new object:
+        // It takes in the parameter (radius) and saves it in a 'field variable'
+        double radius; 
+
+
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+ 
+
+    }
+}
+
+
