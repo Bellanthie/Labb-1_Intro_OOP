1d582906b336fe6ab00ed7300f4efa63628d98a2
Bellanthie <bellanthie@gmail.com>
2025-10-28 15:21:19 +0100

    added commentary to show me step-by-step and to remember what the instructions as of me

--- Labb 1_Intro_OOP/Program.cs
+++ Labb 1_Intro_OOP/Program.cs
@@ -3,6 +3,7 @@
 
     class Circle
     {
+        //comments here
         // 1) place the constructor within the curly brackets
         // 2) the Constructor is like the builder who knows how to create a new object:
         // It takes in the parameter (radius) and saves it in a 'field variable'
